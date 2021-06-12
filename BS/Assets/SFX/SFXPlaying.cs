b39ef94701b0dd3c46bbeb22c2eaa192898ad8f6
TingYi-Chang <tonyshop123@gmail.com>
2021-06-12 13:49:39 +0800

    Add SFX.

---
+++ BS/Assets/SFX/SFXPlaying.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXPlaying : MonoBehaviour
+{
+    public AudioSource Bounce;
+    public AudioSource Hit;
+    
+    public void PlayBounce()
+    {
+        Bounce.Play();
+    }
+    public void PlayHit()
+    {
+        Hit.Play();
+    }
+}
